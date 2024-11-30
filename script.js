5598c89fa4850a86c6ce73307ebd2c9725ca9f45
Amarok-w <Vadimon4k00073@gmail.com>
2024-11-30 16:57:30 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log('ERROR');
+
+const button = document.querySelector('.button');
+const login = document.querySelector('#login');
+const password = document.querySelector('#password');
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  buttonLogic(login.value, password.value);
+});
+
+function buttonLogic(log, pass) {
+
+  const loginPattern = /^[a-zA-Z]{1,8}$/;
+
+  const passwordPattern = /^[A-Za-z0-9!@#$%^&*()_]{4,8}$/;
+
+  if (!loginPattern.test(log) || !passwordPattern.test(pass)) {
+    alert('Ошибка');
+  }
+  else {
+    alert('Успешно');
+  }
+}
